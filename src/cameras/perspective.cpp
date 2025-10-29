c528a9a619b069f22a208506fef205d203fbc089
NhatMinh2208 <minh33haha@gmail.com>
2025-10-29 18:17:10 +0100

    assignment1_1, change renderer name

--- src/cameras/perspective.cpp
+++ src/cameras/perspective.cpp
@@ -18,31 +18,57 @@ public:
         const float fov           = properties.get<float>("fov");
         const std::string fovAxis = properties.get<std::string>("fovAxis");
 
-        NOT_IMPLEMENTED
+        // NOT_IMPLEMENTED
 
         // hints:
         // * precompute any expensive operations here (most importantly
         // trigonometric functions)
         // * use m_resolution to find the aspect ratio of the image
+
+        Vector z = Vector(0.0, 0.0, 1.0);
+        float s_x_norm, s_y_norm;
+        float ratio = (float) m_resolution.x() / m_resolution.y();
+        if (fovAxis == "x") {
+            s_x_norm = tan(fov * Pi / 360.0);
+            s_y_norm = s_x_norm / ratio;
+        } else {
+            s_y_norm = tan(fov * Pi / 360.0);
+            s_x_norm = ratio * s_y_norm;
+        }
+        Vector up = Vector(0.0, 1.0, 0.0);
+        s_x       = up.cross(z);
+        s_y       = z.cross(s_x);
+        s_x       = s_x_norm * s_x.normalized();
+        s_y       = s_y_norm * s_y.normalized();
     }
 
     CameraSample sample(const Point2 &normalized, Sampler &rng) const override {
-        NOT_IMPLEMENTED
+        // NOT_IMPLEMENTED
 
         // hints:
         // * use m_transform to transform the local camera coordinate system
         // into the world coordinate system
+        Vector z   = Vector(0.0, 0.0, 1.0);
+        Vector dir = z + normalized.x() * s_x + normalized.y() * s_y;
+        Ray r      = Ray(Point(0.f), dir.normalized());
+        r          = m_transform->apply(r);
+        return CameraSample{ r.normalized(), Color(1.0) };
     }
 
     std::string toString() const override {
-        return tfm::format("Perspective[\n"
-                           "  width = %d,\n"
-                           "  height = %d,\n"
-                           "  transform = %s,\n"
-                           "]",
-                           m_resolution.x(), m_resolution.y(),
-                           indent(m_transform));
+        return tfm::format(
+            "Perspective[\n"
+            "  width = %d,\n"
+            "  height = %d,\n"
+            "  transform = %s,\n"
+            "]",
+            m_resolution.x(),
+            m_resolution.y(),
+            indent(m_transform));
     }
+
+protected:
+    Vector s_x, s_y;
 };
 
 } // namespace lightwave
