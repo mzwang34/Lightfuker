425600d483e846cf42d0cd0e64f2bd7c9d1764a4
Mengzhu Wang <mzwang3434@gmail.com>
2025-12-27 17:41:02 +0100

    add thin lens camera

---
+++ src/cameras/thinlens.cpp
@@ -0,0 +1,69 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+
+class ThinLens : public Camera {
+public:
+    ThinLens(const Properties &properties) : Camera(properties) {
+        const float fov           = properties.get<float>("fov");
+        const std::string fovAxis = properties.get<std::string>("fovAxis");
+        m_lensRadius              = properties.get<float>("lensRadius", 0.f);
+        m_focalDistance           = properties.get<float>("focalDistance", 1.f);
+
+        Vector z = Vector(0.0, 0.0, 1.0);
+        float s_x_norm, s_y_norm;
+        float ratio = (float) m_resolution.x() / m_resolution.y();
+        if (fovAxis == "x") {
+            s_x_norm = tan(fov * Pi / 360.0);
+            s_y_norm = s_x_norm / ratio;
+        } else {
+            s_y_norm = tan(fov * Pi / 360.0);
+            s_x_norm = ratio * s_y_norm;
+        }
+        Vector up = Vector(0.0, 1.0, 0.0);
+        s_x       = up.cross(z);
+        s_y       = z.cross(s_x);
+        s_x       = s_x_norm * s_x.normalized();
+        s_y       = s_y_norm * s_y.normalized();
+    }
+
+    CameraSample sample(const Point2 &normalized, Sampler &rng) const override {
+        Vector z   = Vector(0.0, 0.0, 1.0);
+        Vector dir = (z + normalized.x() * s_x + normalized.y() * s_y).normalized();
+
+        Point origin(0.f);
+        if (m_lensRadius > 0) {
+            Point2 sLens = squareToUniformDiskConcentric(rng.next2D());
+            Point pLens(m_lensRadius * sLens.x(), m_lensRadius * sLens.y(), 0.f);
+
+            float ft = m_focalDistance / dir.z();
+            Point pFocus = dir * ft;
+
+            origin = pLens;
+            dir = (pFocus - origin).normalized();
+        }
+        Ray r(origin, dir);
+        r = m_transform->apply(r);
+        return CameraSample{ r.normalized(), Color(1.0) };
+    }
+
+    std::string toString() const override {
+        return tfm::format(
+            "ThinLens[\n"
+            "  width = %d,\n"
+            "  height = %d,\n"
+            "  transform = %s,\n"
+            "]",
+            m_resolution.x(),
+            m_resolution.y(),
+            indent(m_transform));
+    }
+
+protected:
+    Vector s_x, s_y;
+    float m_lensRadius, m_focalDistance;
+};
+
+} // namespace lightwave
+
+REGISTER_CAMERA(ThinLens, "thinlens")
